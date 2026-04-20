39d8c9e15ea199a0172c2a932f306b10c20ff38f
Yunus Emre Cevik <y.cevik6161@gmail.com>
2026-04-20 14:44:30 +0200

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+# Seite anzeigen
+def index(request):
+    return render(request, "dashboard/index.html")
+
+
+# Berechnung (Pipeline!)
+def calculate_heat_loss(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        area = float(data.get("area", 0))
+        u_value = float(data.get("u_value", 0))
+
+        heat_loss = area * u_value
+
+        return JsonResponse({
+            "heat_loss": heat_loss
+        })
